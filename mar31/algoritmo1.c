5110b233776e669ed9a250248f2a7ca33a294995
GerardoAlonso230 <fg.alonsogomez@ugto.mx>
2025-04-02 10:38:29 -0600

    Dowhile e ingresar DoWhile

---
+++ mar31/algoritmo1.c
@@ -0,0 +1,36 @@
+ 
+1. Inicio
+ 
+ 
+2. Declarar variables: inicio, fin, incremento
+ 
+ 
+3. Solicitar y leer el valor inicial (inicio)
+ 
+ 
+4. Solicitar y leer el valor final (fin)
+ 
+ 
+5. Solicitar y leer el valor de incremento (incremento)
+ 
+ 
+6. Hacer
+ 
+Imprimir inicio
+ 
+Sumar incremento a inicio
+ 
+ 
+ 
+7. Mientras se cumpla:
+ 
+Si incremento es positivo: inicio ≤ fin
+ 
+Si incremento es negativo: inicio ≥ fin
+ 
+ 
+ 
+8. Imprimir un salto de línea
+ 
+ 
+9. Fin
---
+++ mar31/algoritmo2.c
@@ -0,0 +1,29 @@
+1. Inicio
+ 
+ 
+2. Declarar variable: numero
+ 
+ 
+3. Repetir
+ 
+Mostrar mensaje: "Ingrese un número positivo: "
+ 
+Leer numero
+ 
+Si numero ≤ 0, entonces:
+ 
+Mostrar mensaje: "El número ingresado no es positivo. Ingresa otro."
+ 
+ 
+ 
+ 
+4. Hasta que numero > 0
+ 
+ 
+5. Mostrar mensaje: "El número X es positivo" (donde X es el número ingresado)
+ 
+ 
+6. Fin
+ 
+ 
+ 
