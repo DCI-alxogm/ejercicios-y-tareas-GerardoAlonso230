43c2dd1f19bf4c7ee314a7cde6ea0099c9e70b8c
GerardoAlonso230 <fg.alonsogomez@ugto.mx>
2025-05-26 11:52:20 -0600

    26 de mayo

---
+++ 26dmayo/ejemplo2apuntadores.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void cuadrado_(float *x_ptr, float *x2_ptr);
+
+ int main(){
+ 	int i;
+ 	float x[4],x2[4];
+ 	
+ 	printf("los valores inciales de de los arreglos x y x2 son");
+ 	
+ 	
+ 	for (i=0;i<4;i++){
+ 		printf 
+ 		scanf("%f",&x[i]);
+ 	}
+ 	cuadrado_(x,x2);
+ 	
+ 	for (i=0;i<4;++){
---
+++ 26dmayo/ejercicio1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void cuadrado();
+
+int main (){	
+	cuadrado();
+	return (0);
+}
+	
+void cuadrado (){
+	float x,x2;
+	printf("introducir numero");
+	scanf("%f",&x);
+	x2=x*x;
+	printf("El cuadrado de %f es %f \n",x,x2);
+	
+}
