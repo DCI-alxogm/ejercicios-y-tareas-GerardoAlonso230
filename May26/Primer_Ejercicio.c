b400795854a7a68db2741d72cae83b4185406915
Dulcinea436 <dm.gonzalezpantaleon@ugto.mx>
2025-06-01 19:48:50 -0600

    Primer ejercicio sin argumentos de entrada ni de salida

---
+++ May26/Primer_Ejercicio.c
@@ -0,0 +1,15 @@
+/*Primer ejercicio sin argumentos de entrada ni de salida 
+Gerardo A
+*/
+
+#include <stdio.h>
+
+// Función sin argumentos de entrada ni salida
+void saludar() {
+    printf("¡Hola! Esta función no recibe ni devuelve valores.\n");
+}
+
+int main() {
+    saludar(); // Llamada a la función
+    return 0;
+}
